73e8aaefb5da767ffa82b244ef90026602b3d801
konutcalismalari <107220009+konutcalismalari@users.noreply.github.com>
2023-01-11 23:39:35 +0000

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,9 @@
-/*
-Everything in between is a comment.
-*/
+window.onload = () => {
+    const readBarFill = document.getElementById('read-bar-fill');
+    window.addEventListener('scroll', () => {
+      const elem = document.documentElement, body = document.body;
+      const scrollTop = elem.scrollTop  || body.scrollTop;
+      const scrollBottom = (elem.scrollHeight  || body.scrollHeight) - window.innerHeight;
+      readBarFill.style.width = `${100.0 * scrollTop / scrollBottom}%`;
+    });
+  };
