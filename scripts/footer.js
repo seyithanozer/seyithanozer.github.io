219cd7f291bfc8519a8a47a680b96a55ec3348e3
seyithanozer <52532876+seyithanozer@users.noreply.github.com>
2026-08-04 20:57:52 +0100

    Update footer.js

--- scripts/footer.js
+++ scripts/footer.js
@@ -3,9 +3,9 @@ document.addEventListener('DOMContentLoaded', function () {
   var footer = document.createElement('footer');
   footer.className = 'site-footer';
   footer.innerHTML =
-    '<p class="meta">This page emits ' +
-    '<a href="https://digitalbeacon.co/report/seyithanozer-com">' +
-    '0.068\u00a0g of CO\u2082 per visit' +
+    '<p class="meta">' +
+    '<a href="https://www.websitecarbon.com/website/seyithanozer-com/">' +
+    'Less than 0.01\u00a0g of CO\u2082 per visit' +
     '<span class="arr" aria-hidden="true">&#8599;&#xfe0e;</span>' +
     '</a>.</p>';
   document.body.appendChild(footer);
